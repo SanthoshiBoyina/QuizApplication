985533a3f32b362195d4a0b87781a8115baebf67
SanthoshiBoyina <83487694+SanthoshiBoyina@users.noreply.github.com>
2023-01-06 17:01:14 +0530

    Update Score.java

--- Score.java
+++ Score.java
@@ -9,7 +9,7 @@ public class Score extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/result.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/result.jpg"));
         Image i2 = i1.getImage().getScaledInstance(300, 250, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
